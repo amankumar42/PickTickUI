35b72df436312be782ea132e77318577b1e2b5dc
Aman Kumar <amankumar@amans-macbook-air.local>
2017-05-13 11:08:18 +0530

    Initial Commit

---
+++ PickTickVts02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickTickVts02
+//
+//  Created by Aman Kumar on 5/13/17.
+//  Copyright © 2017 Aman Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
