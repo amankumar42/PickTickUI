48a19a25fab6757fc1ff16ba96f2fb1a26d14ca3
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2017-05-13 13:50:54 +0530

    first commit

---
+++ PickTickVts02/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  PickTickVts02
+//
+//  Created by Aman Kumar on 5/13/17.
+//  Copyright © 2017 Aman Kumar. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
